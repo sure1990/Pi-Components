d2a2aefa101c9931994e28926b851fc6f545a5cc
Suresh Kumar H <sure1990@gmail.com>
2023-12-21 08:18:25 +0530

    db creation started

--- app-electron/src/main-ipc/index.ts
+++ app-electron/src/main-ipc/index.ts
@@ -1,7 +1,12 @@
 import { ipcMain } from "electron";
 import api from "./api";
 
+
 export default () => {
   ipcMain.handle("api", (_, ...args) => {
     return api(args[0], args[1])});
 };
+
+function createDatabase(){
+  
+}
