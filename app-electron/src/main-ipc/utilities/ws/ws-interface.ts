7ec23ff82b602276adc7ef1a0a3ead0564bd7015
Sureshkumar Harikrishnan <Sureshkumar.Harikrishnan@agilysys.com>
2024-01-04 00:39:50 +0530

    error handling added

--- app-electron/src/main-ipc/utilities/ws/ws-interface.ts
+++ app-electron/src/main-ipc/utilities/ws/ws-interface.ts
@@ -2,16 +2,22 @@ import WebSocket from 'ws';
 
 let connection: WebSocket;
 
+let isAvailable = false;
+
 function connect() {
   connection = new WebSocket('ws://vrtdesignspi.local:9000');
 
   connection.onopen = function () {
+    isAvailable = true;
     console.log('Connection established!');
   };
   connection.onclose = function () {
+    isAvailable = false;
+
     console.log('Connection closed!');
   };
   connection.onerror = function () {
+    isAvailable = false;
     console.log('Connection error!');
   };
 }
@@ -21,7 +27,7 @@ function close() {
 }
 
 function send(msg: string) {
-  connection.send(msg);
+  if (isAvailable) connection.send(msg);
 }
 
 const WSInterface = {
